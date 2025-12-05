062e4539268f07c385689e53b1885ffc9f6c5d64
yicichen01 <yicichen@uw.edu>
2025-12-05 15:09:40 -0800

    new update on yici_test.py

--- tests/yici_test.py
+++ tests/yici_test.py
@@ -1,7 +1,7 @@
 # test get_chunk_spikes
 
 import numpy as np
-from CSE583_humanSayMonkeyDo.core import get_chunk_spikes
+from src.CSE583_humanSayMonkeyDo.extractors import get_chunk_spikes
 
 
 def test_yici_smoke_get_chunk_spikes():
